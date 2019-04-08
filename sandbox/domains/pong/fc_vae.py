240cd7af5223af4520af8800cf5051d8631ab7a4
admin <haokuan@mit.edu>
2019-04-08 16:44:29 -0400

    gg

--- sandbox/domains/pong/fc_vae.py
+++ sandbox/domains/pong/fc_vae.py
@@ -1,177 +1,202 @@
+
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import numpy as np
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 import random
-import matplotlib.pyplot as plt
 
-class VAE(nn.Module):
-    def __init__(self, n_feature, n_hidden, loss_type='xentropy', output_type='sigmoid'):
-        super(VAE, self).__init__()
-        assert loss_type in ['xentropy', 'L2']
-        self.loss_type = loss_type
 
-        hidden2 = (n_feature + n_hidden) // 2
-        self.n_feature = n_feature
-        self.n_hidden = n_hidden
 
-        self.fc1 = nn.Linear(n_feature, hidden2)
-        self.fc_mu = nn.Linear(hidden2, n_hidden)
-        self.fc_logvar = nn.Linear(hidden2, n_hidden)
-        self.fc3 = nn.Linear(n_hidden, hidden2)
-        self.fc4 = nn.Linear(hidden2, n_feature)
 
-        self.opt = torch.optim.Adam(self.parameters(), lr=1e-3)
+Conv_W = 3
+CC, LL, WW = 32, 6, 6
 
-        self.output_type = output_type
 
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc_mu(h1), self.fc_logvar(h1)
+def to_torch(x, dtype="float", req=False):
+    tor_type = torch.cuda.LongTensor if dtype == "int" else torch.cuda.FloatTensor
+    x = Variable(torch.from_numpy(x).type(tor_type), requires_grad=req)
+    return x
 
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
 
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        if self.output_type == 'sigmoid':
-            return torch.sigmoid(self.fc4(h3))
-        if self.output_type == 'tanh':
-            return torch.tanh(self.fc4(h3))
-        if self.output_type == 'linear':
-            return self.fc4(h3)
+class CNN(nn.Module):
+    def __init__(self, n_chan):
+        super(CNN, self).__init__()
+        # 1 channel input to 2 channel output of first time print and written
+        self.conv1 = nn.Conv2d(n_chan, 8, Conv_W)
+        self.conv2 = nn.Conv2d(8, 16, Conv_W)
+        self.conv3 = nn.Conv2d(16, 32, Conv_W)
+
+        self.dense_enc = nn.Linear(CC * LL * WW, 100)
+
+        # variational bits
+        self.fc_mu = nn.Linear(100, 32)
+        self.fc_logvar = nn.Linear(100, 32)
+
+        self.dense_dec = nn.Linear(100, CC * LL * WW)
+
+        self.deconv3 = torch.nn.ConvTranspose2d(32, 16, Conv_W)
+        self.deconv2 = torch.nn.ConvTranspose2d(16, 8, Conv_W)
+        self.deconv1 = torch.nn.ConvTranspose2d(8, n_chan, Conv_W)
+
+        self.pool = nn.MaxPool2d(2, return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2)
+
+        self.opt = torch.optim.Adam(self.parameters(), lr=0.001)
 
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        # conv1
+        x = F.relu(self.conv1(x))
+        size1 = x.size()
+        x, idx1 = self.pool(x)
 
-    def ae(self, x):
-        mu, logvar = self.encode(x)
-        return self.decode(mu)
+        # conv2
+        x = F.relu(self.conv2(x))
+        size2 = x.size()
+        x, idx2 = self.pool(x)
+        # print('size2=',size2)
 
-    # Reconstruction + KL divergence losses summed over all elements and batch
-    def loss_function(self, recon_x, x, mu, logvar):
-        BCE = lambda : F.binary_cross_entropy(recon_x, x)
-        L2L = lambda : torch.sum((recon_x - x) ** 2)
+        # conv3
+        x = F.relu(self.conv3(x))
+        size3 = x.size()
+        x, idx3 = self.pool(x)
 
-        REC = BCE() if self.loss_type == 'xentropy' else L2L()
+        # =================================================
+        # reached the middle layer, some dense
+        x = x.view(-1, CC * LL * WW)
+        # x = x.view(-1,8*60*60)
+        x = torch.relu(self.dense_enc(x))
 
+        mu, logvar = self.fc_mu(x), self.fc_logvar(x)
+        x = self.reparameterize(mu, logvar)
 
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        x = F.relu(self.dense_dec(x))
+        x = x.view(-1, CC, LL, WW)
+        # =================================================
 
-        return REC + KLD
+        # deconv3
+        x = self.unpool(x, idx3, size3)
+        x = F.relu(self.deconv3(x))
 
-class FcVAE():
-    def __init__(self, n_feature, n_hidden, loss_type, output_type='sigmoid'):
-        self.name = "FcVAEnet"
-        self.n_feature = n_feature
-        self.n_hidden = n_hidden
-        self.loss_type = loss_type
-        self.output_type = output_type
-        self.vae = None
+        # deconv2
+        x = self.unpool(x, idx2, size2)
+        x = F.relu(self.deconv2(x))
 
-    def save(self, loc):
-        torch.save(self.vae.state_dict(), loc)
-
-    def load(self, loc):
-        vae = VAE(self.n_feature, self.n_hidden).cuda()
-        vae.load_state_dict(torch.load(loc))
-        self.vae = vae
-        return vae
-
-    def torchify(self, x):
-        x = Variable(torch.from_numpy(x).type(torch.cuda.FloatTensor), 
-                     requires_grad = False)
+        # deconv1
+        x = self.unpool(x, idx1, size1)
+        x = torch.sigmoid(self.deconv1(x))
         return x
 
-    def learn(self, X, learn_iter = 100000):
-        vae = VAE(self.n_feature, self.n_hidden, self.loss_type, self.output_type).cuda()
-        losses = []
-            # for i in range(99999999999):
-        for i in range(learn_iter):
-            # load in the datas
-            indices = sorted( random.sample(range(len(X)), 40) )
-            X_sub = X[indices]
-            # convert to proper torch forms
-            X_sub = self.torchify(X_sub)
+    # def decode(self, x):
 
-            # optimize 
-            vae.opt.zero_grad()
-            rec, mu, logvar = vae(X_sub)
-            loss = vae.loss_function(rec, X_sub, mu, logvar)
-            loss.backward()
-            vae.opt.step()
-            losses.append(loss.data.cpu().numpy())
+    def embed(self, x):
+        x = to_torch(x)
+        x = F.relu(self.conv1(x))
+        size1 = x.size()
+        x = F.relu(self.conv2(x))
+        size2 = x.size()
+        x = x.view(-1, CC * LL * WW)
+        x = torch.tanh(self.dense_enc(x))
+        return x
 
-            # # some breaking conditions
-            # if len(losses) > 20000:
-            #     first_loss = losses[0]
-            #     last_loss = losses[-1]
-            #     if last_loss * 10000 < first_loss:
-            #         break
+    # VAE MAGIC =================
 
-            #     far_loss = sum(losses[-20000:-10000]) 
-            #     near_loss = sum(losses[-10000:])
-            #     if near_loss > 0.98 * far_loss:
-            #         break
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+    def learn_once(self, imgs):
+        img_rec = self(imgs)
 
-            # if len(losses) % len(X) == 0:
-            #     print ("loss ", len(losses), loss)
-            if i % 4000 == 0:
-                print (i, loss)
-
-        self.vae = vae
-        return vae
-
-    def learn_once(self, X_sub):
-        if self.vae is None:
-            self.vae = VAE(self.n_feature, self.n_hidden, self.loss_type, self.output_type).cuda()
-
-        # X_sub = self.torchify(X_sub)
-
-        vae = self.vae
-        # optimize 
-        vae.opt.zero_grad()
-        rec, mu, logvar = vae(X_sub)
-        loss = vae.loss_function(rec, X_sub, mu, logvar)
+        self.opt.zero_grad()
+        # compute all the cost LOL
+        loss = ((img_rec - imgs) ** 2).mean()
         loss.backward()
-        vae.opt.step()
+
+        for param in self.parameters():
+            param.grad.data.clamp_(-1, 1)
+
+        self.opt.step()
         return loss
 
-    def embed(self, X):
-        if self.vae is None:
-            self.vae = VAE(self.n_feature, self.n_hidden, self.loss_type, self.output_type).cuda()
-        X = np.array(X)
-        X = self.torchify(X)
-        encoded, _, = self.vae.encode(X)
-        return encoded.data.cpu().numpy()
+    def save(self, save_path):
+        torch.save(self.state_dict(), save_path)
+
+    def load(self, loc):
+        self.load_state_dict(torch.load(loc))
+
+    def learn(self, X, learn_iter=1000):
+        losses = []
+        # for i in range(99999999999):
+        for i in range(learn_iter):
+            # load in the datas
+            indices = sorted(random.sample(range(len(X)), 40))
+            # indices = list(range(40))
+            X_sub = X[indices]
+            # convert to proper torch forms
+            X_sub = to_torch(X_sub)
+
+            # optimize
+            losses.append(self.learn_once(X_sub).data.cpu().numpy())
+
+            if i % 1000 == 0:
+                print(i, losses[len(losses) - 1])
+                #img_orig = X_sub[0].detach().cpu().numpy()
+                #img_rec = self(X_sub)[0].detach().cpu().numpy()
+
+                #draw(img_orig, 'orig_img.png')
+                #draw(img_rec, 'rec_img.png')
+
+
+def project(X, dim=32, loop=10000):
+    T = random_projection.GaussianRandomProjection(n_components=dim)
+
+    X_new = []
+    for i in range(0, X.shape[0], loop):
+        X_new.append(T.fit_transform(X[i:i + loop]))
+    X_new = np.vstack(X_new)
+
+    return X_new
+
+
+def flatten(X):
+    s = X.shape
+    s_new = (s[0], s[1] * s[2] * s[3])
+    return np.reshape(X, s_new)
+
+
+def get_X_Y(filename):
+    with open(filename, 'rb') as f:
+        X = pickle.load(f)
+        Y = pickle.load(f)
+    return X, Y
+
+
+def save_X_Y(filename, X, Y):
+    with open(filename, 'wb') as f:
+        pickle.dump((X, Y), f, protocol=4)
+    return
+
 
 
 if __name__ == '__main__':
-    print ('embedding mnist now')
+    print ('embedding ppo2 now')
     from data import gen_train_data, gen_test_data, WSampler
-    X_tr, Y_tr = gen_train_data(list(range(60000)))
-    X_tr = X_tr.reshape(-1, 28*28)
+    X_tr, Y_tr = get_X_Y('/baselines/baselines/ppo2_memory_obs_actions')
+    X_tr = np.transpose(X_tr,(0,3,1,2))
 
     emb_dim = 32
 
     import pickle
 
     X_size = X_tr.shape[1]
-    vae = FcVAE(X_size, emb_dim, 'L2', output_type='linear')
+    vae = CNN(4)
 
     vae.learn(X_tr, learn_iter = 100000)
 
     # compute the embedded features
     X_tr_emb = vae.embed(X_tr)
 
-    data_embed_path = 'mnist_emb_{}.p'.format(emb_dim)
+    data_embed_path = 'pong_emb_{}.p'.format(emb_dim)
     pickle.dump((X_tr_emb,Y_tr), open( data_embed_path, "wb" ) )
