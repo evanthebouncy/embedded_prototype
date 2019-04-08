24669b56ec11f7cfc0aab8b50dee81850ab74bdb
Haokuan Luo <haokuan@sketch3.csail.mit.edu>
2019-04-08 15:18:42 -0400

    gg

--- sandbox/subset_selection/random_proj.py
+++ sandbox/subset_selection/random_proj.py
@@ -7,10 +7,14 @@ X_new = T.fit_transform(X)
 print (X_new.shape)
 print (X.shape)
 
-def project(X,dim = 32):
+def project(X,dim = 32,loop = 10000):
     T = random_projection.GaussianRandomProjection(n_components=dim)
-
-    X_new = T.fit_transform(X)
+    
+    X_new = []
+    for i in range(0,X.shape[0],loop):
+        X_new.append(T.fit_transform(X[i:i+loop]))
+    X_new = np.vstack(X_new)
+    
     return X_new
 
 def flatten(X):
@@ -23,11 +27,15 @@ def get_X_Y(filename):
         X=pickle.load(f)
         Y=pickle.load(f)
     return X,Y
-
+def save_X_Y(filename,X,Y):
+    with open(filename,'wb') as f:
+        pickle.dump((X,Y),f,protocol=4)
+    return
 if __name__ == '__main__':
-    X,Y = get_X_Y('domains/pong/baselines/baselines/ppo2_memory')
+    X,Y = get_X_Y('domains/pong/baselines/baselines/ppo2_memory_obs_actions')
     print(X.shape,Y.shape)
     X_flat=flatten(X)
     X_emb=project(X_flat)
     print(X_emb.shape)
+    save_X_Y('domains/pong/baselines/baselines/ppo2_memory_dim32',X_emb,Y)
 
