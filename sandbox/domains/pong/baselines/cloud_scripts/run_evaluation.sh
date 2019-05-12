40b7a117f7f5ca82a99d89fea70be8c1ef4f567e
evanthebouncy <evanthebouncy@gmail.com>
2019-05-12 17:00:34 -0400

    stuf

--- sandbox/domains/pong/baselines/cloud_scripts/run_evaluation.sh
+++ sandbox/domains/pong/baselines/cloud_scripts/run_evaluation.sh
@@ -1,2 +1,2 @@
-python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v4 --num_timesteps=1e6 --mode=evaluate_pretrain --index_path=../pong_data/idx_all.p --memory_path=../pong_data/ppo2_memory --loss_path=../pong_data/result_loss_idx_all
+python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v4 --num_timesteps=8e6 --mode=evaluate_pretrain --index_path=../pong_data/idx_none.p --memory_path=../pong_data/ppo2_memory_1e5 --loss_path=../pong_data/result_idx_none
 
