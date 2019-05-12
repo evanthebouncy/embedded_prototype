f5aefe6ba55f222c29d67e5996cf1ac597d68d67
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-12 20:08:45 +0000

    no shutdown for now

--- sandbox/domains/pong/baselines/cloud_scripts/run_evaluation.sh
+++ sandbox/domains/pong/baselines/cloud_scripts/run_evaluation.sh
@@ -1,3 +1,2 @@
 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v4 --num_timesteps=1e6 --mode=evaluate_pretrain --index_path=../pong_data/idx_all.p --memory_path=../pong_data/ppo2_memory --loss_path=../pong_data/result_loss_idx_all
-sudo poweroff
 
